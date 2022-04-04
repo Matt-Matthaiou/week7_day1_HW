f0623be511903cd77dce32fcda769b947fd5894b
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-04-04 17:27:32 +0100

    end of hw

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function App() 
+{
+
+  const [toDoList, setToDoList] = useState(
+    [
+      {name:"Cook dinner", priority: false},
+      {name: "Clean dishes", priority: true}
+    ]);
+
+  const [newTask, setNewTask] = useState()
+
+  const taskList = toDoList.map((task, index) =>
+  {
+    return(
+      <li key={index}>
+        <span style={task.priority ? {color:'red'} :{color:'green'}}>{task.name}</span>
+      </li>
+    )
+  });
+
+  const handleInput = (event)=>
+  {
+    setNewTask(event.target.value);
+  }
+
+  const handleForm = (event)=>
+  {
+    event.preventDefault();
+    const copyTasks = toDoList;
+    console.log(event.target.priority.value);
+    const createdTask = {name: newTask, priority:checkPriority(event.target.priority.value)};
+    copyTasks.push(createdTask);
+    setNewTask('')
+  }
+
+  const checkPriority = (value)=>
+  {
+    if (value === 'true')
+    {
+      return true;
+    }
+    else if (value === 'false')
+    {
+      return false;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ToDo's</h1>
+      <form onSubmit={handleForm}>
+        <label htmlFor='new-to-do'>Add a new task</label>
+        <input id='new-to-do' type='text' onChange={handleInput} value={newTask}/>
+        <label htmlFor='priority'>Low priority</label>
+        <input name='priority' type='radio' value='false'/>
+        <label htmlFor='priority'>high priority</label>
+        <input name='priority' type='radio' value='true'/>
+        <input type='submit' value='Add task'></input>
+      </form>
+      <ul>
+        {taskList}
+      </ul>
+    </>
   );
 }
 
